2d6b7a006fd3e0958503bf51db7acb303df89581
juliette2000 <71046863+juliette2000@users.noreply.github.com>
2023-08-06 18:40:11 -0400

    Create User.java

---
+++ app/src/main/java/com/example/firetds/User.java
@@ -0,0 +1,28 @@
+package com.example.firetds;
+
+public class User {
+    private String username;
+    private String password;
+
+    public User() {
+
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword() {
+        this.password = password;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
